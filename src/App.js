adb97adf37ca4da57ae6da20a65b487a73411a0a
Lana <60522454+SSeuraj22@users.noreply.github.com>
2023-06-16 00:56:52 +0000

    Installed material ui icons and added a few icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +28,12 @@ function App() {
         <div>
           <Button variant="contained">Hello World</Button>
         </div>
+        <div>
+          <Icon>star</Icon>
+          <Icon>add_circle</Icon>
+          <Icon>elderly</Icon>
+        </div>
+
       </header>
     </div>
   );
